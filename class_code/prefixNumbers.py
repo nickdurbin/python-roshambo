fdc7e287052f18ccc965d8cae266be44e60219ed
nick.durbin <nick.durbin@gmail.com>
2020-10-04 12:59:39 -0400

    new solution to meet optimization.

--- class_code/prefixNumbers.py
+++ class_code/prefixNumbers.py
@@ -1,13 +1,38 @@
-def prefixFreePhones(numbers):
-    numbers.sort()
-    print(numbers)
-    for i in range(len(numbers)): 
-        for j in range(i+1, len(numbers)):
-            if numbers[j].startswith(numbers[i]):
-                return False
-            if numbers[i].startswith(numbers[j]):
-                return False
+# def prefixFreePhones(numbers):
+#     numbers.sort()
+#     print(numbers)
+#     for i in range(len(numbers)): 
+#         for j in range(i+1, len(numbers)):
+#             if numbers[j].startswith(numbers[i]):
+#                 return False
+#             if numbers[i].startswith(numbers[j]):
+#                 return False
 
-    return True 
+#     return True 
 
 # Solution needs work
+def prefixFreePhones(numbers):
+
+    numbers.sort()
+
+    for n in range(len(numbers) -1):
+        if numbers[n+1].startswith(numbers[n]):
+            print(numbers[n+1], numbers[n])
+            return False
+        
+
+    return True
+
+numbers = ["69297543", 
+ "692975867", 
+ "692975799", 
+ "69297578",
+ "692975222", 
+ "692975380", 
+ "692975279", 
+ "69297547",
+ "692975884",
+ "69297502", 
+ "69297525"]
+
+print(prefixFreePhones(numbers))
