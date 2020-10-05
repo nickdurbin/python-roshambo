f19cad84e23e32b6b85abbd1788e2f9bc0e24bda
durbcodes <nick.durbin@gmail.com>
2020-10-05 04:15:27 -0400

    Binary practice.

---
+++ binary/canvas_exam.py
@@ -0,0 +1,31 @@
+# In a binary (base 2) number system, the places are sequential powers of 2:
+
+# 2^0 = 1
+# 2^1 = 2
+# 2^2 = 4
+# 2^3 = 8
+# So, what if "101" was in binary? What would it represent then? Well, if we read it right to left, we have a "1" in the ones place, a "0" in the twos place, and a "1" in the fours place. We add these values up (4 + 0 + 1) which equals 5.
+
+# Below, is a table that shows how to count up to 12 in binary and decimal:
+
+# Decimal	Binary
+# 0	0000
+# 1	0001
+# 2	0010
+# 3	0011
+# 4	0100
+# 5	0101
+# 6	0110
+# 7	0111
+# 8	1000
+# 9	1001
+# 10	1010
+# 11	1011
+# 12	1100
+# Challenge
+# Convert the following decimal numbers into binary numbers:
+
+# 25 0001 1001
+# 63 0011 1111
+# 9 1001
+# 111 0110 1110
