dc3ec63c72d9ee5c001900cc1579c0d214454dcc
nick.durbin <nick.durbin@gmail.com>
2020-10-06 20:13:06 -0400

    modified file.

--- class_code/prefixNumbers.py
+++ class_code/prefixNumbers.py
@@ -38,3 +38,36 @@ numbers = ["69297543",
  "69297525"]
 
 print(prefixFreePhones(numbers))
+
+# def prefixFreePhones(numbers):
+#     # overall runtime is O(n^2)
+#     # for each number in numbers: 
+#     for num in numbers: # O(n)
+#     # compare it to every other number: 
+#         for other_num in numbers:   # O(n)
+#             # if one of the numbers if a prefix of the other
+#             if num == other_num:
+#                 continue
+#             if other_num.startswith(num) or num.startswith(other_num): 
+#             # return true
+#                 return False
+#     return True
+
+
+# def prefixFreePhones(numbers):
+#     # what if we sort numbers first? 
+#     # numbers is a list of strings, so it's going sort alphabetically
+#     # ["911", "9112345", "9876543"]
+#     # overall runtime : O(n log n)
+#     # sort
+#     numbers.sort()      # O(n log n)
+#     # iterate through
+#     for i in range(len(numbers) - 1):       # O(n)
+#     # compare each element with the one right after it
+#         first = numbers[i]
+#         second = numbers[i+1]
+#     # if the second element starts with the first, return false
+#         if second.startswith(first):
+#             return False
+#     return True
+#     # otherwise return True
