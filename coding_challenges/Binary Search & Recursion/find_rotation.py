72cf12f8712728577f28da0f819d80d56d16c6ac
nick.durbin <nick.durbin@gmail.com>
2020-10-21 22:01:24 -0400

    Added some new problems for BS and recusion.

---
+++ coding_challenges/Binary Search & Recursion/find_rotation.py
@@ -0,0 +1,86 @@
+"""
+I was bored one day and decided to look at last names in the phonebook for my
+area.
+​
+I flipped open the phonebook to a random page near the middle and started
+perusing. I wrote each last name that I was unfamiliar with down on paper in
+increasing order. When I got to the end of the phonebook, I was having so much
+fun I decided to start from the beginning and keep going until I reached the
+page where I had started.
+​
+When I was finished, I had a list of interesting last names that were mostly
+alphabetical. The problem was that my list starts somewhere near the middle of
+the alphabet, reaches the end, and then starts from the beginning of the
+alphabet. In other words, my list of names is sorted, but it is "rotated."
+​
+Example:
+​
+surnames = [
+    'sparks',
+    'zhang',
+    'ahmed',  # <-- rotates here!
+    'brandt',
+    'davenport',
+    'farley',
+    'glover',
+    'kennedy',
+    'liu',
+    'mcdowell',
+    'nixon',
+]
+​
+[ 6, 7, 8, 15, 30, 0, 1, 2, 3, 4, 5]
+​
+Write a function that finds the index of the "rotation point". The "rotation
+point" is where I started working from the beginning of the phone book. The
+list I came up was absolutely huge, so make sure your solution is efficient.
+​
+*Note: you should be able to come up with a solution that has O(log n) time
+complexity.*
+"""
+def find_rotation_point(surnames):
+    # Your code here
+​
+    # UNDERSTAND
+​
+    # [ 6,   7, 8, 0, 1, 2, 3, 4, 5]
+    #           min max
+    #           mid
+​
+    # [ 7, 0, 1,  2, 3, 4, 5, 6]
+         #  min
+         # max
+         # mid
+    # Plan:
+    # we should use some kind of a binary search
+​
+    # max and min indices
+    min = 0
+    max = len(surnames) - 1
+    # find the middle index
+    mid = (min + max) // 2
+​
+    # if the middle surname is the rotation point:  (base case)
+        # middle surname is rotation if prev element is > than current
+        # return the index
+    # otherwise:
+    # have we rotated yet?
+    # we can tell that by comparing to the first element in the array
+    first_element = surnames[0]
+​
+    while not max <= min:
+        current_element = surnames[mid]
+        # if current element >= first element: we haven't rotated yet, rotation point is "still ahead of us"
+        if current_element >= first_element:
+            # search right
+            # move the min index to middle + 1
+            min = mid + 1
+        # else if current element < first element: we have rotated
+        else:
+            # current_element < first_element
+            # search left:
+            # move the max index to middle index
+            max = mid
+​
+    # keep going until max and min cross
+    return min
---
+++ coding_challenges/Binary Search & Recursion/first_draft.py
@@ -0,0 +1,56 @@
+"""
+You are a new author that is working on your first book. You are working on a
+series of drafts. Each draft is based on the previous draft. The latest draft
+of your book has a serious typo. Since each newer draft is based on the
+previous draft, all the drafts after the draft containing the typo also include
+the typo.
+​
+Suppose you have `n` drafts `[1, 2, 3, ..., n]` and you need to find out the
+first one containing the typo (which causes all the following drafts to have
+the typo as well).
+​
+You are given access to an API tool `containsTypo(draft)` that will return
+`True` if the draft contains a typo and `False` if it does not.
+​
+You need to implement a function that will find the *first draft that contains
+a typo*. Also, you have to pay a fee for every call to `containsTypo()`, so
+make sure that your solution minimizes the number of API calls.
+​
+Example:
+​
+Given `n = 5`, and `draft = 4` is the first draft containing a typo.
+​
+containsTypo(3) -> False
+containsTypo(5) -> True
+containsTypo(4) -> True
+"""
+def firstDraftWithTypo(n):
+    # Your code here
+    # [ draft1, draft2, draft3, ... draft n-1, draft n]
+​
+    # If we find a draft with the typo, the first draft with the typo (target) is before the current draft
+    # if current draft does not have the typo, the first draft w/ the typo is after the current draft
+​
+    # start w/ min and max
+    min = 0
+    max = n
+​
+    while not max <= min:
+        guess = (min + max) // 2
+        contains_typo = containsTypo(guess)
+        if contains_typo:
+            # the first draft w/ the typo is before our guess
+            # look left
+            # we could check if it's the first draft by calling containsTypo(guess - 1)
+            # if not containsTypo(guess - 1):
+            #     return guess
+            # move max to the guess value
+            max = guess - 1
+        else:
+            # current draft doesn't have a typo
+            # so the first draft w/ typo is after our guess
+            # look right
+            # move min to guess + 1
+            min = guess + 1
+    # repeat
+    return min
