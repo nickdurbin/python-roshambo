d99e75ae353e71bde84f68371a9a57410fc56a08
nick.durbin <nick.durbin@gmail.com>
2020-11-11 21:46:10 -0500

    Added the max Lambdas problem.

--- coding_challenges/Canvas GCA Prac/mergeStrings.py
+++ coding_challenges/Canvas GCA Prac/mergeStrings.py
@@ -3,8 +3,6 @@ You are implementing your own programming language and you've decided to add sup
 
 For example, if s1 = "super" and s2 = "tower", the result should be merge(s1, s2) = "stouperwer".
 
-
-
 You'd like to make your language more unique, so for your merge function, instead of comparing the characters in the usual lexicographical order, you'll compare them based on how many times they occur in their respective strings (fewer occurrences means the character is considered smaller). If the number of occurrences are equal, then the characters should be compared in the usual lexicographical way. If both number of occurences and characters are equal, you should take the characters from the first string to the result.
 
 Given two strings s1 and s2, return the result of the special merge function you are implementing.
--- coding_challenges/Random Challenges/max_number_lambdas.py
+++ coding_challenges/Random Challenges/max_number_lambdas.py
@@ -1,3 +1,27 @@
+'''
+Given a string text, you need to use the characters of text to form as many instances of the word "lambda" as possible.
+
+You can use each character in text at most once.
+
+Write a function that returns the maximum number of instances of "lambda" that can be formed.
+
+Example 1:
+
+Input: text = "mbxcdatlas"
+Output: 1
+Example 2:
+
+Input: text = "lalaaxcmbdtsumbdav"
+Output: 2
+Example 3:
+
+Input: text = "sctlamb"
+Output: 0
+Notes:
+
+text consists of lowercase English characters only
+'''
+
 def csMaxNumberOfLambdas(text):
   num_of_times = 0
   num_l = text.count("l")
