3957ec76f65346648439d78e3909d4d0ed1e6597
nick.durbin <nick.durbin@gmail.com>
2020-11-12 22:00:08 -0500

    Added the frequency questions to the hash tables folder.

--- coding_challenges/Random Challenges/max_number_lambdas.py
+++ coding_challenges/Random Challenges/max_number_lambdas.py
@@ -45,3 +45,50 @@ def csMaxNumberOfLambdas(text):
 # for i in the_string:
 #     text_list.append(text.count(i))
 # return(min(text_list))
+
+# def csMaxNumberOfLambdas(text):
+#     # UNDERSTAND
+#     # return an int
+#     # all we care about is the number of times the characters "l", "a" (x 2 or divided by 2), "m", "b", "d" appear in text
+#     # build a dictionary with "l"/"a"/ etc... as the keys, and their frequency in text as the values
+#     lambda_counts = {}
+#     for char in text: 
+#         if char not in "lambda": 
+#             continue
+#         if char in lambda_counts: 
+#             lambda_counts[char] += 1
+#         else: 
+#             lambda_counts[char] = 1
+#     # look at the values for each char and return the minimum ( divide by 2 for a)
+#     min_value = None
+#     for char, count in lambda_counts.items(): 
+#         if char == "a": 
+#             count = count // 2 # integer division, if there are 3 a's, can only make 1 lambda
+#         if min_value is None or count < min_value: 
+#             min_value = count
+#     return min_value    
+# def csMaxNumberOfLambdas_original(text):
+#     lambda_counts = {}
+#     for char in text: 
+#         if char not in "lambda": 
+#             continue
+#         if char in lambda_counts: 
+#             lambda_counts[char] += 1
+#         else: 
+#             lambda_counts[char] = 1
+#     num_lambdas = 0
+#     while True: 
+#         made_lambda = True
+#         for char in "lambda": 
+#             if char not in lambda_counts: 
+#                 made_lambda = False
+#                 break
+#             lambda_counts[char] -= 1
+#             if lambda_counts[char] < 0:
+#                 made_lambda = False
+#                 break
+#         if made_lambda: 
+#             num_lambdas += 1
+#         else: 
+#             break
+#     return num_lambdas
