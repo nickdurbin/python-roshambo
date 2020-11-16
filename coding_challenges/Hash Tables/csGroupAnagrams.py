5067b38dd95de5ecd77a4576f61c7825f158b28d
nick.durbin <nick.durbin@gmail.com>
2020-11-15 20:39:30 -0500

    Solution to the anagrams.

--- coding_challenges/Hash Tables/csGroupAnagrams.py
+++ coding_challenges/Hash Tables/csGroupAnagrams.py
@@ -34,4 +34,46 @@ strs[i] consists of lower-case English letters.
 [output] array.array.string
 '''
 
+from collections import defaultdict
+
 def csGroupAnagrams(strs):
+  # Solution from reading Geeks
+  # DefaultDict is a collection built in to Python
+  # It is a custom DS and we pass it list and hold it in a var
+  temp_list = defaultdict(list)
+  # Iterate over our list of words
+  for el in strs:
+      # Sort and append to our temporary list
+      temp_list[str(sorted(el))].append(el)
+  # Put all of the values from the temp into a sorted list
+  sorted_array = list(temp_list.values())
+
+  return sorted_array
+
+
+# list comprehension answer
+# using list comprehension + sorted() + lambda + groupby()
+# Grouping Anagrams
+# from itertools import groupby
+# temp = lambda test_list: sorted(test_list)
+# res = [list(val) for key, val in groupby(sorted(test_list, key = temp), temp)]
+
+# Solution 3
+#def createAnagramKey(string):
+#     key = ''
+#     for ch in sorted(string):
+#         key += ch
+#     return str(key)
+ 
+ 
+# def groupAnagramWords(data):
+#     group = dict()
+#     for ele in data:
+#         if group.get(createAnagramKey(ele)) == None:
+#             group[createAnagramKey(ele)] = [ele]
+#         else:
+#             group[createAnagramKey(ele)].append(ele)
+#     return group
+ 
+ 
+# anagram_grouped = groupAnagramWords(data)
